12fa0f5a73ed8935f79c06c9367a2ea02e474666
Arka Jyoti Adhikary <arkajyotiadhikary15@gmail.com>
2021-11-18 01:00:59 +0530

    [client]/body-design

--- yoga_client/src/Components/Body/Body.js
+++ yoga_client/src/Components/Body/Body.js
@@ -1,9 +1,21 @@
+import IOTStates from "./IOTStates";
+import WorkoutStates from "./WorkoutStates";
+
 const Body = () => {
     return (
         <div className="container-fluid">
             <div className="row body-row">
-                <div className="col-8 col-body-right "></div>
-                <div className="col-4 col-body-left"></div>
+                <div className="col-3 col-body-left ">
+                    <div className="d-flex justify-content-start mt-5 ms-2">
+                        <WorkoutStates />
+                    </div>
+                </div>
+                <div className="col-6 col-body-middle my-4"></div>
+                <div className="col-3 col-body-right my-4 ">
+                    <div className="d-flex justify-content-center align-items-center h-100 ">
+                        <IOTStates />
+                    </div>
+                </div>
             </div>
         </div>
     );
---
+++ yoga_client/src/Components/Body/IOTStates.js
@@ -0,0 +1,21 @@
+const IOTStates = () => {
+    return (
+        <div className="card  bg-transparent border-0">
+            <div className="card-body">
+                <h5 className="card-title">Heart Beat</h5>
+                <p className="card-text">0</p>
+                <h5 className="card-title">Blood Presure</h5>
+                <p className="card-text">0</p>
+                <h5 className="card-title">Oxygen Saturation</h5>
+                <p className="card-text">0</p>
+                <h5 className="card-title">Tempreture</h5>
+                <p className="card-text">0</p>
+            </div>
+            <div className="card-footer bg-transparent border-0 mt-5">
+                Date: Nov 5, 2021, 8:15:30 am
+            </div>
+        </div>
+    );
+};
+
+export default IOTStates;
