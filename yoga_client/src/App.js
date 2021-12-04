46edc756b3aecc2b8c317067ccc6a103cd7c323b
ManishShah120 <mkshah141@gmail.com>
2021-12-04 12:56:58 +0530

    [refactor] Fix the dir name and import issue

--- yoga_client/src/App.js
+++ yoga_client/src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 
-import {} from "react-router";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// TODO when `Link` will be used remove the above line and uncomment the below line
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import React from "react";
-// componets
+// component
 import WorkoutWindow from "./components/workout_window/WorkoutWindow";
 import Main from "./components/main/Main";
-import Signup from "./components/Sign-up/Signup";
+import Signup from "./components/sign-up/Signup";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 function App() {
