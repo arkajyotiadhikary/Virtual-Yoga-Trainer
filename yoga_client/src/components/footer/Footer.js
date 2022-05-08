7431121b7e80696a99cda0cc189b4bd8ea20d246
Arka Jyoti Adhikary <arkajyotiadhikary15@gmail.com>
2022-05-08 17:47:52 +0530

    [client] changed posenet and implement pose detection using tensorflow

--- yoga_client/src/components/footer/Footer.js
+++ yoga_client/src/components/footer/Footer.js
@@ -1,11 +1,11 @@
 const Footer = () => {
     return (
-        <footer class="site-footer mt-5">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12 col-md-6">
+        <footer className="site-footer mt-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12 col-md-6">
                         <h6>About</h6>
-                        <p class="text-justify">
+                        <p className="text-justify">
                             Ea rebum sadipscing lorem nonumy duo sed elitr amet
                             lorem dolore. Sadipscing erat et diam stet. Sea
                             justo magna voluptua accusam. Nonumy kasd sadipscing
@@ -16,9 +16,9 @@ const Footer = () => {
                         </p>
                     </div>
 
-                    <div class="col-xs-6 col-md-3">
+                    <div className="col-xs-6 col-md-3">
                         <h6>Categories</h6>
-                        <ul class="footer-links">
+                        <ul className="footer-links">
                             <li>
                                 <a href="http://scanfcode.com/category/c-language/">
                                     af
@@ -52,9 +52,9 @@ const Footer = () => {
                         </ul>
                     </div>
 
-                    <div class="col-xs-6 col-md-3">
+                    <div className="col-xs-6 col-md-3">
                         <h6>Quick Links</h6>
-                        <ul class="footer-links">
+                        <ul className="footer-links">
                             <li>
                                 <a href="http://scanfcode.com/about/">
                                     About Us
@@ -85,34 +85,34 @@ const Footer = () => {
                 </div>
                 <hr />
             </div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 col-sm-6 col-xs-12">
-                        <p class="copyright-text">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 col-sm-6 col-xs-12">
+                        <p className="copyright-text">
                             Copyright &copy; 2021 All Rights Reserved
                         </p>
                     </div>
 
-                    <div class="col-md-4 col-sm-6 col-xs-12">
-                        <ul class="social-icons">
+                    <div className="col-md-4 col-sm-6 col-xs-12">
+                        <ul className="social-icons">
                             <li>
-                                <a class="facebook" href="/#">
-                                    <i class="fa fa-facebook"></i>
+                                <a className="facebook" href="/#">
+                                    <i className="fa fa-facebook"></i>
                                 </a>
                             </li>
                             <li>
-                                <a class="twitter" href="/#">
-                                    <i class="fa fa-twitter"></i>
+                                <a className="twitter" href="/#">
+                                    <i className="fa fa-twitter"></i>
                                 </a>
                             </li>
                             <li>
-                                <a class="dribbble" href="/#">
-                                    <i class="fa fa-dribbble"></i>
+                                <a className="dribbble" href="/#">
+                                    <i className="fa fa-dribbble"></i>
                                 </a>
                             </li>
                             <li>
-                                <a class="linkedin" href="/#">
-                                    <i class="fa fa-linkedin"></i>
+                                <a className="linkedin" href="/#">
+                                    <i className="fa fa-linkedin"></i>
                                 </a>
                             </li>
                         </ul>
--- yoga_client/src/components/navbar/ProfileCard.js
+++ yoga_client/src/components/navbar/ProfileCard.js
@@ -1,9 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 const ProfileCard = () => {
     return (
-        <div class="dropdown profile-card">
+        <div className="dropdown profile-card">
             <a
-                class="btn btn-secondary bg-transparent border-0"
+                className="btn btn-secondary bg-transparent border-0"
                 href="#"
                 role="button"
                 id="dropdownMenuLink"
@@ -18,36 +18,36 @@ const ProfileCard = () => {
                 />
             </a>
             <div
-                class="dropdown-menu dropdown-menu-end p-0"
+                className="dropdown-menu dropdown-menu-end p-0"
                 data-bs-popper="none"
             >
-                <div class="dropdown-item card p-0">
-                    <div class="card-header">
+                <div className="dropdown-item card p-0">
+                    <div className="card-header">
                         <div className="d-flex justify-content-center">
                             <img
-                                class="mx-auto  "
+                                className="mx-auto  "
                                 src="https://t3.ftcdn.net/jpg/03/91/19/22/360_F_391192211_2w5pQpFV1aozYQhcIw3FqA35vuTxJKrB.jpg"
                                 alt="profile_picture"
                                 height="80px"
                             />
                         </div>
-                        <h1 class="card-title text-center h3">Arka</h1>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item bg-transparent border-0">
+                        <h1 className="card-title text-center h3">Arka</h1>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item bg-transparent border-0">
                                 <a className="nav-link p-0" href="">
                                     Profile
                                 </a>
                             </li>
-                            <li class="list-group-item bg-transparent border-0">
+                            <li className="list-group-item bg-transparent border-0">
                                 <a className="nav-link p-0">Report</a>
                             </li>
-                            <li class="list-group-item bg-transparent border-0">
+                            <li className="list-group-item bg-transparent border-0">
                                 <a className="nav-link p-0">Change Password</a>
                             </li>
                         </ul>
                     </div>
-                    <div class="card-footer bg-transparent">
-                        <button class="btn btn-sm btn-outline-primary text-end">
+                    <div className="card-footer bg-transparent">
+                        <button className="btn btn-sm btn-outline-primary text-end">
                             Logout
                         </button>
                     </div>
