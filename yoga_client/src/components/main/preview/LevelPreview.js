2e66d49819a83b8bdda650b709d9b3fa57db17bd
Arka Jyoti Adhikary <arkajyotiadhikary15@gmail.com>
2022-05-27 03:38:05 +0530

    Add texts and button in choose your plan carousals

--- yoga_client/src/components/main/preview/LevelPreview.js
+++ yoga_client/src/components/main/preview/LevelPreview.js
@@ -1,6 +1,6 @@
 const LevelPreview = () => {
     return (
-        <div className="">
+        <div className="level-preview">
             <h2>Choose Your Plan</h2>
             <div
                 id="carouselExampleIndicators"
@@ -8,28 +8,6 @@ const LevelPreview = () => {
                 data-bs-ride="carousel"
             >
                 <div className="carousel-inner">
-                    <div className="carousel-indicators">
-                        <button
-                            type="button"
-                            data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide-to="0"
-                            className="active"
-                            aria-current="true"
-                            aria-label="Slide 1"
-                        ></button>
-                        <button
-                            type="button"
-                            data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide-to="1"
-                            aria-label="Slide 2"
-                        ></button>
-                        <button
-                            type="button"
-                            data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide-to="2"
-                            aria-label="Slide 3"
-                        ></button>
-                    </div>
                     <div className="controll">
                         <button
                             className="carousel-control-prev"
@@ -57,25 +35,73 @@ const LevelPreview = () => {
                         </button>
                     </div>
                     <div className="carousel-item active">
-                        <img
-                            src="https://media.istockphoto.com/photos/african-trainer-and-group-of-people-meditating-during-yoga-class-picture-id1222262916?b=1&k=20&m=1222262916&s=170667a&w=0&h=-yKDc2yXo5mJ8SDpyKP-Rz91bR68o9rS9N8g-q7Oak8="
-                            className="d-block w-100"
-                            alt="..."
-                        />
+                        <div className="item-inside">
+                            <div
+                                className="d-flex justify-content-center align-items-center "
+                                style={{
+                                    position: "absolute",
+                                    height: "100%",
+                                    width: "100%",
+                                }}
+                            >
+                                <button className="level-selector btn">
+                                    Beginner
+                                </button>
+                            </div>
+                            <img
+                                height={"520rem"}
+                                style={{ objectFit: "cover" }}
+                                src="https://media.istockphoto.com/photos/african-trainer-and-group-of-people-meditating-during-yoga-class-picture-id1222262916?b=1&k=20&m=1222262916&s=170667a&w=0&h=-yKDc2yXo5mJ8SDpyKP-Rz91bR68o9rS9N8g-q7Oak8="
+                                className="d-block w-100"
+                                alt="..."
+                            />
+                        </div>
                     </div>
                     <div className="carousel-item">
-                        <img
-                            src="https://media.istockphoto.com/photos/yoga-reverse-warrior-pose-picture-id483465412?b=1&k=20&m=483465412&s=170667a&w=0&h=JBkrata6ox3NikqvL0x-z8lBu30DQkfTKK9NWGPmYQI="
-                            className="d-block w-100"
-                            alt="..."
-                        />
+                        <div className="item-inside">
+                            <div
+                                className="d-flex justify-content-center align-items-center "
+                                style={{
+                                    position: "absolute",
+                                    height: "100%",
+                                    width: "100%",
+                                }}
+                            >
+                                <button className="level-selector btn">
+                                    Intermidiate
+                                </button>
+                            </div>
+                            <img
+                                height={"520rem"}
+                                style={{ objectFit: "cover" }}
+                                src="https://media.istockphoto.com/photos/strong-male-yogi-standing-on-his-hands-with-both-feet-in-the-air-picture-id1283571490?b=1&k=20&m=1283571490&s=170667a&w=0&h=2T7cT5XAQZRloSIXri6khH9Vo_szVtiVJx-lvCBe4XE="
+                                className="d-block w-100"
+                                alt="..."
+                            />
+                        </div>
                     </div>
                     <div className="carousel-item">
-                        <img
-                            src="https://media.istockphoto.com/photos/strong-male-yogi-standing-on-his-hands-with-both-feet-in-the-air-picture-id1283571490?b=1&k=20&m=1283571490&s=170667a&w=0&h=2T7cT5XAQZRloSIXri6khH9Vo_szVtiVJx-lvCBe4XE="
-                            className="d-block w-100"
-                            alt="..."
-                        />
+                        <div className="item-inside">
+                            <div
+                                className="d-flex justify-content-center align-items-center "
+                                style={{
+                                    position: "absolute",
+                                    height: "100%",
+                                    width: "100%",
+                                }}
+                            >
+                                <button className="level-selector btn">
+                                    Expert
+                                </button>
+                            </div>
+                            <img
+                                height={"520rem"}
+                                style={{ objectFit: "cover" }}
+                                src="https://media.istockphoto.com/photos/yoga-reverse-warrior-pose-picture-id483465412?b=1&k=20&m=483465412&s=170667a&w=0&h=JBkrata6ox3NikqvL0x-z8lBu30DQkfTKK9NWGPmYQI="
+                                className="d-block w-100"
+                                alt="..."
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
